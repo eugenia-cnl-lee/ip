23e47d3ecd4ebf018fc1e13920bc306ad66acdbe
Chun Nga Lee <leeeugenia16@gmail.com>
2026-02-05 02:03:53 +0800

    Rename chatbot and add basic greeting and exit
    The chatbot's name is Duke and so is the main source file name.
    The main source file prints out a 'Duke' logo.
    
    It needs to change because,
    * the chatbot's name needs to differentiate from other chatbots' names
    * just printing the logo does not help achieve the goal of the chatbot
      being a personal assistant which helps the user keep track of various
      things
    
    Let's,
    * rename the chatbot to Athena
    * rename Duke.java to Athena.java
    * delete the chatbot logo
    * implement an initial skeletal version (with horizontal lines seperating
      the starting and ending greetings) of the chatbot to greet the user
      and exit
    
    It is done this way because,
    * Athena is not a common name choice, hence differentiating it a lot
    * for consistency's sake and to prevent potential future bugs
    * horizontal lines will make formatting for future code and main body
      content prompts easier and nicer, laying a good foundation for a
      skeleton code

---
+++ src/main/java/Athena.java
@@ -0,0 +1,12 @@
+public class Athena {
+    public static void main(String[] args) {
+        String horizontalLine = "\n___________-___________________";
+
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Athena");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
