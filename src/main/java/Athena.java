b394159ae15ec191b7a35b1e7ca2ec76f3810b2b
Chun Nga Lee <leeeugenia16@gmail.com>
2026-02-05 09:07:36 +0800

    Standardise code style to match Java conventions

--- src/main/java/Athena.java
+++ src/main/java/Athena.java
@@ -1,6 +1,6 @@
 public class Athena {
     public static void main(String[] args) {
-        Greetings.hello();
-        Greetings.goodbye();
+        Greetings.showGreeting();
+        Greetings.showExit();
     }
 }
--- src/main/java/Greetings.java
+++ src/main/java/Greetings.java
@@ -1,15 +1,15 @@
 public class Greetings {
-    // Greeting Message
-    public static void hello() {
-        System.out.println("Hello wisdom-seeker! I'm Athena, your personal assistant AI bot.");
+    // Prints the welcome message when the user starts the program
+    public static void showGreeting() {
+        System.out.println("Hello wisdom-seeker! I am Athena, your personal assistant chatbot.");
         System.out.println("I am named after the Greek goddess of wisdom, as I possess all the worldly knowledge.");
         System.out.println("Thus, I am able to assist you in all ways.");
         System.out.println("What can I do for you, my curious child?");
         System.out.println("=========================================================");
     }
 
-    // Goodbye Message
-    public static void goodbye() {
+    // Prints the goodbye message when the user exits the program
+    public static void showExit() {
         System.out.println("You no longer require my assistance? Then I shall take my leave now.");
         System.out.println("I hope my teachings of today reside with you forever.");
         System.out.println("May your thirst for knowledge be as fierce and as eternal as Greek Fire.");
