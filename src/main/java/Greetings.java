b6bc4ea267e695e99108e7c157e8e83f3d12b3b8
Chun Nga Lee <leeeugenia16@gmail.com>
2026-02-05 08:41:54 +0800

    Added personality to the bot

--- src/main/java/Greetings.java
+++ src/main/java/Greetings.java
@@ -1,12 +1,17 @@
 public class Greetings {
     // Greeting Message
     public static void hello() {
-        System.out.println("Hello! I'm Athena");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello wisdom-seeker! I'm Athena, your personal assistant AI bot.");
+        System.out.println("I am named after the Greek goddess of wisdom, as I possess all the worldly knowledge.");
+        System.out.println("Thus, I am able to assist you in all ways.");
+        System.out.println("What can I do for you, my curious child?");
+        System.out.println("=========================================================");
     }
 
     // Goodbye Message
     public static void goodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("You no longer require my assistance? Then I shall take my leave now.");
+        System.out.println("I hope my teachings of today reside with you forever.");
+        System.out.println("May your thirst for knowledge be as fierce and as eternal as Greek Fire.");
     }
 }
