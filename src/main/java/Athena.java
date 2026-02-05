24939298b18f3a7da6622735a625a8afc66dc4ca
Chun Nga Lee <leeeugenia16@gmail.com>
2026-02-05 08:16:11 +0800

    Turn into OOP style

--- src/main/java/Athena.java
+++ src/main/java/Athena.java
@@ -1,12 +1,6 @@
 public class Athena {
     public static void main(String[] args) {
-        String horizontalLine = "\n___________-___________________";
-
-        System.out.println(horizontalLine);
-        System.out.println("Hello! I'm Athena");
-        System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        Greetings.hello();
+        Greetings.goodbye();
     }
 }
---
+++ src/main/java/Greetings.java
@@ -0,0 +1,12 @@
+public class Greetings {
+    // Greeting Message
+    public static void hello() {
+        System.out.println("Hello! I'm Athena");
+        System.out.println("What can I do for you?");
+    }
+
+    // Goodbye Message
+    public static void goodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
